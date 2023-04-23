542c90fe67e2af7481dc0f8c4870209942c787cf
steveo45 <steve@alkimiya.io>
2023-04-23 04:29:28 -0700

    Add handler for processWithdrawsEvent and populate fields

--- src/SilicaVault.ts
+++ src/SilicaVault.ts
@@ -5,7 +5,22 @@ import {
     SilicaVault as SilicaVaultEntity
 } from "../generated/schema";
   
-import { StartNextRound } from "../generated/SilicaVault/SilicaVault";
+import { StartNextRound, ProcessWithdraws } from "../generated/SilicaVault/SilicaVault";
+
+export function handleProcessWithdraws(event: ProcessWithdraws): void {
+    let silicaVault = SilicaVaultEntity.load(event.address.toHex());
+    if (!silicaVault) {
+        silicaVault = new SilicaVaultEntity(event.address.toHex());
+        silicaVault.save();
+    }
+
+    // Store values for next epoch
+    let curEpochEntry = new Epoch(event.params.currentRound.toString());
+    curEpochEntry.paymentHeldAtWithdraw = event.params.pTokenWithdrawn;
+    curEpochEntry.rewardHeldAtWithdraw = event.params.rTokenWithdrawn;
+    curEpochEntry.sharesWithdrawn = event.params.sharesWithdrawn;
+    curEpochEntry.save();   
+}
 
 export function handleStartNextRound(event: StartNextRound): void {
     let silicaVault = SilicaVaultEntity.load(event.address.toHex());
@@ -16,25 +31,16 @@ export function handleStartNextRound(event: StartNextRound): void {
 
     // Store values for next epoch
     let curEpochEntry = new Epoch(event.params.currentRound.toString());
-    curEpochEntry.startTimestamp = event.block.timestamp;
-    curEpochEntry.roundEnd = event.params.roundEndDay;
-    curEpochEntry.roundSize = event.params.roundSize;
-    curEpochEntry.deposited = event.params.deposited;
-    curEpochEntry.sharePriceAtStart = event.params.sharePriceAtStart;
-    curEpochEntry.shareSupplyAtStart = event.params.shareSupplyAtStart;
-    curEpochEntry.silicaVaultV1 = event.address.toHex();
-    curEpochEntry.save();
-
-    // Update fields for previous epoch
-    let prevEpoch = event.params.currentRound.minus(new BigInt(1));
-    let prevEpochEntry = EpochEntity.load(prevEpoch.toString());
-    if (!prevEpochEntry) {
-        // handle null gracefully
+    if (!curEpochEntry) {
+        // exit gracefully
     } else {
-        prevEpochEntry.paymentHeldAtWithdraw = event.params.paymentHeldAtWithdraw;
-        prevEpochEntry.rewardHeldAtWithdraw = event.params.rewardHeldAtWithdraw;
-        prevEpochEntry.sharesWithdrawn = event.params.sharesWithdrawn;
-        prevEpochEntry.save();   
+        curEpochEntry.startTimestamp = event.block.timestamp;
+        curEpochEntry.roundEnd = event.params.roundEndDay;
+        curEpochEntry.roundSize = event.params.roundSize;
+        curEpochEntry.deposited = event.params.deposited;
+        curEpochEntry.sharePriceAtStart = event.params.sharePriceAtStart;
+        curEpochEntry.shareSupplyAtStart = event.params.shareSupplyAtStart;
+        curEpochEntry.silicaVaultV1 = event.address.toHex();
+        curEpochEntry.save();
     }
-
 }
